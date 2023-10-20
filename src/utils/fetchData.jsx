72ac3b62c4ce15969541c2150be59eff5dbeea25
spanzy teo <spanzytheo@gmail.com>
2023-10-20 17:21:11 +0100

    added videos for the exercise
    description

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -13,3 +13,11 @@ export const fetchData = async (url, options) => {
 
   return data
 }
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+}
